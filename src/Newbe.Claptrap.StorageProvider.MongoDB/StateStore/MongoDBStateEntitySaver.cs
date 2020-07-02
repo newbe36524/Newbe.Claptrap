acc86e82254f68901367200ba85722c64b1ce6cd
newbe36524 <newbe36524@qq.com>
2020-07-02 12:20:05 +0800

    1

--- src/Newbe.Claptrap.StorageProvider.MongoDB/StateStore/MongoDBStateEntitySaver.cs
+++ src/Newbe.Claptrap.StorageProvider.MongoDB/StateStore/MongoDBStateEntitySaver.cs
@@ -63,9 +63,7 @@ namespace Newbe.Claptrap.StorageProvider.MongoDB.StateStore
             var upsertModels = items.Select(x =>
             {
                 var filter = new ExpressionFilterDefinition<MongoStateEntity>(entity =>
-                    entity.claptrap_id == x.claptrap_id
-                    && entity.claptrap_type_code == x.claptrap_type_code
-                    && entity.version > x.version);
+                    entity.claptrap_id == x.claptrap_id && entity.claptrap_type_code == x.claptrap_type_code);
                 return new ReplaceOneModel<MongoStateEntity>(filter, x)
                 {
                     IsUpsert = true,
